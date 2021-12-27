5f6b979297b0ab90e1bd6cfc81b7c3b7e46400e7
Ken Snyder <ken@ken.net>
2021-12-27 08:23:45 -0800

    fix: move aware from peer deps

--- src/@types/proxies.ts
+++ src/@types/proxies.ts
@@ -1,4 +1,5 @@
 import { TypeGuard as TG } from "inferred-types";
-export { TypeSubtype } from "common-types";
+import { TypeSubtype as TS } from "common-types";
 
 export type TypeGuard<T> = TG<T>;
+export type TypeSubtype<T extends string, S extends string> = TS<T, S>;
