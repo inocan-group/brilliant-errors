8d40b08b7d65ccd2b155257940ece2076c913008
Ken Snyder <ken@ken.net>
2022-11-17 09:13:49 -0800

    chore: switched vitest config to use ESM __dirname

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,19 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import { resolve } from "pathe";
+import { fileURLToPath } from "node:url";
+
+// const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 // used for testing, library code uses TSUP to build exports
 export default defineConfig({
+  resolve: {
+    alias: {
+      src: resolve(__dirname, "./src"),
+      test: resolve(__dirname, "./test"),
+    }
+  },
   test: {
     dir: "test",
   },
